c9b465612f49b436769834126d7ecf313918d41d
quantum-corn <akashkhetan044@gmail.com>
2022-01-19 00:16:17 +0530

    first base tables creation

--- dbcreate.sql
+++ dbcreate.sql
@@ -1,7 +1,8 @@
 CREATE DATABASE vaxer;
 USE vaxer;
-CREATE TABLE login;
-CREATE TABLE registration;
-CREATE TABLE vaccines;
-CREATE TABLE centres;
-CREATE TABLE records;
+CREATE TABLE login (email VARCHAR(50) PRIMARY KEY, password VARCHAR(25));
+CREATE TABLE registration(uidai BIGINT PRIMARY KEY, centre INT, slot INT);
+CREATE TABLE vaccines(vacc_id INT PRIMARY KEY, name VARCHAR(25), status CHAR(1));
+CREATE TABLE centres(center_id INT PRIMARY KEY, name VARCHAR(50), address VARCHAR(1000), district VARCHAR(50), state VARCHAR(20), pincode INT);
+CREATE TABLE uidai(uidai BIGINT PRIMARY KEY, first_name VARCHAR(25), last_name VARCHAR(25), age INT, gender CHAR(1));
+CREATE TABLE records(uidai BIGINT PRIMARY KEY, dose INT);
