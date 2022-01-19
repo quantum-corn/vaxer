093365810b53079e5a3ca09c2b142634b83d07e2
quantum-corn <akashkhetan044@gmail.com>
2022-01-19 22:24:04 +0530

    updated database schema

--- dbcreate.sql
+++ dbcreate.sql
@@ -3,12 +3,8 @@ USE vaxer;
 # for patient to login into the interface
 CREATE TABLE login (email VARCHAR(50) PRIMARY KEY, password VARCHAR(25));
 # for patient to register for the vaccination
-CREATE TABLE registration(uidai BIGINT PRIMARY KEY, centre INT, slot INT);
+CREATE TABLE registration(uidai BIGINT PRIMARY KEY, first_name VARCHAR(25), last_name VARCHAR(25), age INT, gender ENUM('M','F'), type INT, centre INT, slot INT);
 # a list of vaccine options with availability
 CREATE TABLE vaccines(vacc_id INT PRIMARY KEY, name VARCHAR(25), status CHAR(1));
 # a list of vaccination centres
 CREATE TABLE centres(center_id INT PRIMARY KEY, name VARCHAR(50), address VARCHAR(1000), district VARCHAR(50), state VARCHAR(20), pincode INT);
-# a replica of aadhar database
-CREATE TABLE uidai(uidai BIGINT PRIMARY KEY, first_name VARCHAR(25), last_name VARCHAR(25), age INT, gender ENUM('M','F'));
-# vaccination record
-CREATE TABLE records(uidai BIGINT PRIMARY KEY, dose INT);
