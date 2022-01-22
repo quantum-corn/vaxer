70590134d1e46cecb5377f2806635eda4ce21b68
Dolip04 <aninditachakraborty983@gmail.com>
2022-01-22 22:29:56 +0530

    this

--- binaryfile_trial.py
+++ binaryfile_trial.py
@@ -1,7 +1,11 @@
 import pickle
-def write():
-    f= open("binaryfile.dat", "wb")
-    list=["Covishield", "Covaxin", "Sputnik", "ZyCoV-D"]
-    pickle.dump(list, f)
-    f.close()
-write()
+f=open('centers text.txt', 'r')
+f1=open('centers text.dat', 'wb')
+while True:
+    line=f.readline()
+    for word in line.split():
+        word=word.replace('_', '  ')
+        pickle.dump(word,f1)
+        pickle('\n',f1)
+f.close()      
+            
