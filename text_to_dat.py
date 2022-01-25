4396c67b745fb48d49aa06bd95c587bb521ade4c
quantum-corn <akashkhetan044@gmail.com>
2022-01-25 13:43:16 +0530

    basic database integration

--- text_to_dat.py
+++ text_to_dat.py
@@ -3,8 +3,10 @@ f=open('vaccines_text.txt', 'r')
 f1=open('vaccines.dat', 'wb')
 rows=f.readlines()
 for row in rows:
-    for word in row.split():
-        word=word.replace('_', '  ')
-    pickle.dump(row,f1)
+    words=row.split()
+    new=[]
+    for i in range(len(words)):
+        new.append(words[i].replace('_', '  '))
+    pickle.dump(new,f1)
 f.close()
 f1.close()
--- vaxer.py
+++ vaxer.py
@@ -14,6 +14,14 @@ import mysql.connector as sql
 db=sql.connect(host='localhost', user='ash', password='autobotsrollout')
 cursor=db.cursor()
 
+# %% create the database
+def createdb():
+    source=open('dbcreate.sql')
+    lines=source.readlines()
+    for line in lines:
+        if line[0]!='#':
+            cursor.execute(line)
+
 # %% update static data tables
 def update():
     files=('vaccines.dat', 'centers.dat')
@@ -22,15 +30,16 @@ def update():
         while True:
             try:
                 data=pickle.load(table)
-                dataset=data.split()
 
                 if file=='vaccines.dat':
-                    cursor.execute('INSERT INTO vaccines (vacc_id, name, status) VALUES ({0}, {1}, {2});'.format(dataset[0], dataset[1], "'y'" if dataset[2]=='Available' else "'n'"))
+                    cursor.execute('INSERT INTO vaccines (vacc_id, name, status) VALUES ({0}, "{1}", "{2}");'.format(data[0], data[1], 'y' if data[2]=='Available' else 'n'))
                 else:
-                    cursor.execute('INSERT INTO centers (center_id, name, address, district, state, pincode) VALUES ({0}, {1}, {2}, {3}, {4}, {5});'.format(dataset[0], dataset[1], dataset[2], dataset[3], dataset[4], dataset[5]))
+                    cursor.execute('INSERT INTO centers (center_id, name, address, district, state, pincode) VALUES ({0}, "{1}", "{2}", "{3}", "{4}", {5});'.format(data[0], data[1], data[2], data[3], data[4], data[5]))
 
             except EOFError:
                 break
+
+    db.commit()
 
 # %% tkinter root
 root=Tk()
@@ -132,15 +141,10 @@ def admin():
 
 
 
-cursor.execute('SOURCE dbcreate.sql')
+createdb()
 update()
-
-
-
-
-
-
-
 greet()
 
 root.mainloop()
+
+db.close()
